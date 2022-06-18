b6e1b87ba9d08431e2a36309207bd7a9cf300c93
Diogo de Souza <diogosrpriv@gmail.com>
2022-06-17 21:31:59 -0300

    feat: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      }
+    },
+  },
+  plugins: [],
+}
